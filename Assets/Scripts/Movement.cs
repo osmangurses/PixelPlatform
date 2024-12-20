81e8abbe42c7b4463f69fadf2a107d095d6360ad
osmangurses <osmanileiletisim@gmail.com>
2024-12-20 21:34:09 +0300

    Lesson 10

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -8,7 +8,6 @@ public class Movement : MonoBehaviour
     public int speed;
     public int jumpForce;
 
-
     Rigidbody2D body;
     float moveDirection = 0;
     bool isGrounded;
@@ -50,23 +49,26 @@ public class Movement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        body.velocity = new Vector3(moveDirection*speed, body.velocity.y,0);
+        if (!isDeath)
+        {
+            body.velocity = new Vector3(moveDirection * speed, body.velocity.y, 0);
 
-        if (moveDirection>0)
-        {
-            GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else if (moveDirection<0)
-        {
-            GetComponent<SpriteRenderer>().flipX = true;
+            if (moveDirection > 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            else if (moveDirection < 0)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
-            Destroy(GetComponent<Rigidbody2D>());
             isDeath = true;
+            Destroy(GetComponent<Rigidbody2D>());
             GetComponent<Animator>().Play("Death");
             Invoke("ReloadScene", 0.5f);
         }
